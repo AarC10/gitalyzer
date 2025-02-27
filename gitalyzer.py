a7e253cb15dbf5ecfafc99e76e41ae43e351c560
Aaron Chan <aarchan108@gmail.com>
2025-02-27 17:11:00 -0500

    Amended to make stats gathering a function. Gather stats and print them

--- gitalyzer.py
+++ gitalyzer.py
@@ -1,6 +1,7 @@
 import sys
 import git
 import matplotlib.pyplot as plt
+from collections import defaultdict
 
 def main():
     if len(sys.argv) < 2:
@@ -13,6 +14,30 @@ def main():
     except Exception as e:
         print(f"Error opening repository at {repo_path}: {e}")
         sys.exit(1)
+
+    contributions_by_year = defaultdict(int)
+    contributors_by_year = defaultdict(set)
+    overall_contributor_commits = defaultdict(int)
+    contributor_yearly_commits = defaultdict(lambda: defaultdict(int))
+
+    for commit in repo.iter_commits('--all'):
+        year = commit.authored_datetime.year
+        author = commit.author.name
+        
+        contributions_by_year[year] += 1
+        contributors_by_year[year].add(author)
+        overall_contributor_commits[author] += 1
+        contributor_yearly_commits[author][year] += 1
+
+    # Prepare data for aggregate graphs
+    years = sorted(contributions_by_year.keys())
+    total_commits = [contributions_by_year[year] for year in years]
+    total_contributors = [len(contributors_by_year[year]) for year in years]
+
+    print(years)
+    print(total_commits)
+    print(total_contributors)
+
     
 
 if __name__ == "__main__":
